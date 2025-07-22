3ef05abde8a3adc5980b5ded8f77f97c0a1cdcd4
wangxg <wangxg@nocode.com>
2025-07-22 15:15:58 +0800

    feat: 添加 Heading component 并更新页面布局

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,11 @@
 import Link from "next/link";
+import { Heading } from "@/components/Heading";
 import { ticketsPath } from "@/paths";
 
 const HomePage = () => {
   return (
     <div className="flex flex-col gap-y-8">
-      <div>
-        <h2 className="text-3xl font-bold tracking-tight">HomePage</h2>
-        <p className="text-sm text-muted-foreground">
-          Your home place to start
-        </p>
-      </div>
-
+      <Heading title="HomePage" description="Your home place to start" />
 
       <div className="flex flex-col items-center">
         <Link href={ticketsPath()} className="underline">Go to Tickets</Link>
